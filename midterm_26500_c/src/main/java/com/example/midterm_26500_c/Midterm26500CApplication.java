ce7ab93e2be604dbfe8287cb42523e8072cba840
Preacher-Y <ysheja@gmail.com>
2026-03-11 12:48:36 +0200

    Initial Commit

---
+++ midterm_26500_c/src/main/java/com/example/midterm_26500_c/Midterm26500CApplication.java
@@ -0,0 +1,13 @@
+package com.example.midterm_26500_c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Midterm26500CApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Midterm26500CApplication.class, args);
+	}
+
+}
---
+++ midterm_26500_c/src/test/java/com/example/midterm_26500_c/Midterm26500CApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.midterm_26500_c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Midterm26500CApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
