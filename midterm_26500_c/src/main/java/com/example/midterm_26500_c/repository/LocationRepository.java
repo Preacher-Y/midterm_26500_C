b9b3efe487183346cd38cd6a3a5c3a776a7b39a3
Preacher-Y <ysheja@gmail.com>
2026-03-12 18:31:51 +0200

    Small Change on the location thingy

--- midterm_26500_c/src/main/java/com/example/midterm_26500_c/repository/LocationRepository.java
+++ midterm_26500_c/src/main/java/com/example/midterm_26500_c/repository/LocationRepository.java
@@ -5,11 +5,12 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.midterm_26500_c.enums.LocationType;
 
 public interface LocationRepository extends JpaRepository<Location, UUID> {
     boolean existsByCode(String code);
-    boolean existsByTypeAndParent_IdAndName(com.example.midterm_26500_c.enums.LocationType type, UUID parentId, String name);
-    boolean existsByTypeAndParentIsNullAndName(com.example.midterm_26500_c.enums.LocationType type, String name);
+    boolean existsByTypeAndParent_IdAndName(LocationType type, UUID parentId, String name);
+    boolean existsByTypeAndParentIsNullAndName(LocationType type, String name);
     Optional<Location> findByCode(String code);
     List<Location> findByName(String name);
 }
